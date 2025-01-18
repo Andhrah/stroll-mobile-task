d8ccf42078ce03d1af1f217e61eb6f6ee2bc4219
Alexandra Collins <andracolexa@gmail.com>
2025-01-18 12:43:26 +0100

    bonfire - implement a simple cache for user selection

--- lib/features/bonfire/ui/widgets/question_card.dart
+++ lib/features/bonfire/ui/widgets/question_card.dart
@@ -41,7 +41,7 @@ class QuestionCard extends StatelessWidget {
           children: <Widget>[
             Container(
               padding: const EdgeInsets.all(2.0),
-              margin: EdgeInsets.fromLTRB(0, 0, 8.0, 0),
+              margin: EdgeInsets.fromLTRB(0, 0, 5.0, 0),
               width: 25.0,
               height: 25.0,
               decoration: BoxDecoration(
